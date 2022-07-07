dc12267f50abbe87b8c3068bc10c25468850b1a6
Eamonn de Leastar <edeleastar@wit.ie>
2022-07-07 07:17:17 +0100

    updated project build options from vite@latest

--- tutors-course-reader/vite.config.js
+++ tutors-course-reader/vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite'
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var vite_1 = require("vite");
+var vite_plugin_svelte_1 = require("@sveltejs/vite-plugin-svelte");
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-  optimizeDeps: {
-  },
-  server :{
-    port:5000,
-    host: true
-  }
-})
+exports.default = (0, vite_1.defineConfig)({
+    plugins: [(0, vite_plugin_svelte_1.svelte)()],
+    server: {
+        port: 5000,
+        host: true
+    }
+});
