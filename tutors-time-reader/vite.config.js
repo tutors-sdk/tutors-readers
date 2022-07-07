51c22185e1a75340f8d10c7c68eb248a4dbafc2c
Eamonn de Leastar <edeleastar@wit.ie>
2022-07-07 07:26:16 +0100

    updated build options from vite@latest

--- tutors-time-reader/vite.config.js
+++ tutors-time-reader/vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  optimizeDeps: {
-  },
-  server :{
-    port:5000,
-    host: true
-  }
-})
+    plugins: [svelte()],
+    optimizeDeps: {},
+    server: {
+        port: 5000,
+        host: true
+    }
+});
