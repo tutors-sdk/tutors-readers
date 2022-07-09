5286db2cc342537cec52b0a304b6d2037c434e2f
Eamonn de Leastar <edeleastar@wit.ie>
2022-07-09 08:26:56 +0100

    first draft of shared library

---
+++ tutors-reader-lib/src/iconography/IconBar.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Icon from "./Icon.svelte";
+  import type { IconNavBar } from "../types/icon-types";
+
+  export let nav: IconNavBar;
+</script>
+
+<div>
+  {#each nav.bar as i}
+    <span>
+      <a href="{i.link}" target="{i.target}">
+      <Icon toolTip="{i.tip}" type="{i.icon}" button="true" tipPos="tooltip-top"/>
+      </a>
+    </span>
+  {/each}
+</div>
